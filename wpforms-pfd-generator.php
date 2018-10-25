e8c05564921e44f708b902d4b9159ae411efddf7
Tetyana <iluminatuweb@gmail.com>
2018-10-25 11:48:43 +0200

    Update wpforms-pfd-generator.php

---
+++ wpforms-pfd-generator.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Plugin Name: WPForms PDF Generator
+Description: Plugin that extends functionallity of WPForms and extraxts all entry data to a PDF, so you can attach it at a confirmation mail.
+Version: 1.0
+Author: Tetyana Yavorska
+*/
